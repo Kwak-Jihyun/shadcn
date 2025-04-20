a9f83385dcd76861a772d9d1790de8369668892d
Jihyun Kwak <zethor@gmail.com>
2025-04-20 20:01:08 +0900

    type error corrected

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,11 @@ const nextConfig = {
     output: 'export',
 };
 
+const repoName = 'shadcn'; // 본인 레포지토리명으로 변경
+module.exports = {
+  output: 'export',
+  basePath: '/' + repoName,
+  assetPrefix: 'https://kwak-jihyun.github.io/' + repoName,
+};
+
 export default nextConfig;
